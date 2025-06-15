d2968e06e517819e3d34c75f324f8ec4a885e8da
yeeYash <thakareyash99258@gmail.com>
2025-06-15 19:46:47 +0530

    animated text going horizontal

--- script.js
+++ script.js
@@ -7,11 +7,23 @@ gsap.to('.projects-skill-learning', {
     yPercent: -100,
     stagger: 0.5,
     scrollTrigger: {
-        trigger: '.projects-skill-learning',
+        trigger: '.projects-skill-learning', 
         markers: true,
         start: 'top top',
         end: '+=1700px',
         scrub: true,
         pin: true,
+    }
+})
+
+gsap.to('.project', {
+    x: 500,
+    scrollTrigger: {
+        trigger: '.course',
+        markers: true,
+        start: 'top top',
+        end: '+=300px',
+        scrub: true,
+        pin: true
     }
 })
