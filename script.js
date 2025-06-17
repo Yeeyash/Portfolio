6b27fa289a33683ace525495078f6adf5bbfb3de
yeeYash <thakareyash99258@gmail.com>
2025-06-17 11:02:15 +0530

    Horizontal Scroll from now, styles remaining

--- script.js
+++ script.js
@@ -1,29 +1,55 @@
 gsap.registerPlugin(ScrollTrigger);
 
 
-gsap.set('.projects-skill-learning', {position: 'absolute'});
+// gsap.set('.projects-skill-learning', {position: 'absolute'});
 
-gsap.to('.projects-skill-learning', {
-    yPercent: -100,
-    stagger: 0.5,
-    scrollTrigger: {
-        trigger: '.projects-skill-learning', 
-        markers: true,
-        start: 'top top',
-        end: '+=1700px',
-        scrub: true,
-        pin: true,
-    }
-})
+// gsap.to('.projects-skill-learning', {
+//     yPercent: -100,
+//     stagger: 0.5,
+//     scrollTrigger: {
+//         trigger: '.projects-skill-learning', 
+//         markers: true,
+//         start: 'top top',
+//         end: '+=1700px',
+//         scrub: true,
+//         pin: true,
+//     }
+// })
 
-gsap.to('.project', {
-    x: 500,
-    scrollTrigger: {
-        trigger: '.course',
-        markers: true,
-        start: 'top top',
-        end: '+=300px',
-        scrub: true,
-        pin: true
-    }
+// gsap.to('.project', {
+//     x: -500,
+//     y: -200,
+//     scrollTrigger: {
+//         trigger: '.course',
+//         markers: true,
+//         start: 'top top',
+//         end: '+=200',
+//         scrub: true,
+//         pin: true
+//     }
+// })
+
+const divv = document.querySelector('aside .aside');
+
+function amounttoscroll(){
+    let divWidth = divv.scrollWidth;
+    return -(divWidth - window.innerWidth);
+}
+
+const tween = gsap.to('aside .aside', {
+    x: amounttoscroll,
+    // scale: 2,
+    ease: 'none'
+});
+
+ScrollTrigger.create({
+    trigger: 'aside .aside',
+    start: 'top top',
+    end: () => `+=${amounttoscroll() * -1}`,
+    // end: '+=1000px',
+    pin: true,
+    animation: tween,
+    scrub: 1,
+    invalidateOnRefresh: true,
+    markers: true
 })
