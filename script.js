c811dd47967ced0b3e6e4033434d10775d78ae5e
yeeYash <thakareyash99258@gmail.com>
2025-06-24 20:17:06 +0530

    Added colors to shortinfo.

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ ScrollTrigger.create({
 })
 
 const colortween = gsap.to('.gsapskill', {
-    // backgroundImage: 'linear-gradient(135deg, hsla(198, 59%, 82%, 1) 29%, hsla(60, 57%, 91%, 1) 62%, hsla(33, 100%, 88%, 1) 100%)',
     backgroundImage: 'linear-gradient(135deg, hsla(198, 59%, 82%, 1) 15%, hsla(198, 59%, 82%, 1) 23%, hsla(60, 57%, 91%, 1) 52%, hsla(44, 76%, 90%, 1) 76%, hsla(33, 100%, 88%, 1) 100%)',
     ease: 'power1.out'
 })
@@ -34,9 +33,23 @@ const colortween = gsap.to('.gsapskill', {
 ScrollTrigger.create({
     trigger: 'aside .aside',
     start: 'top top',
-    end: '+=400px',
+    end: '+=300px',
     animation: colortween,
     scrub: true,
+    markers: true
+})
+
+const parenttween = gsap.to('.infocontainer', {
+    backgroundImage: 'linear-gradient(135deg, hsla(198, 59%, 82%, 1) 15%, hsla(198, 59%, 82%, 1) 23%, hsla(60, 57%, 91%, 1) 52%, hsla(44, 76%, 90%, 1) 76%, hsla(33, 100%, 88%, 1) 100%)',
+    ease: 'power1.out'
+})
+
+ScrollTrigger.create({
+    trigger: 'aside .aside',
+    start: 'top top',
+    end: '+=50px',
+    animation: parenttween,
+    scrub: 1,
     markers: true
 })
 
@@ -48,8 +61,26 @@ const goodcolortween = gsap.to('.goodat', {
 ScrollTrigger.create({
     trigger: 'aside .aside',
     start: 'top top',
-    end: '+=400px',
+    end: '+=200px',
     animation: goodcolortween,
+    scrub: 1,
+    markers: true
+})
+
+const gsaptween = gsap.to('.gsapskill', {
+    // color: 'linear-gradient(90deg, hsla(60, 56%, 91%, 1) 0%, hsla(81, 24%, 67%, 1) 10%, hsla(86, 21%, 58%, 1) 17%, hsla(90, 22%, 47%, 1) 39%, hsla(99, 44%, 39%, 1) 64%, hsla(101, 63%, 34%, 1) 86%, hsla(104, 99%, 28%, 1) 100%)',
+    // document.querySelector('.gsapskill').style.color: 'linear-gradient(to right, blue, yellow, green)',
+    ease: 'power3.out'
+})
+
+ScrollTrigger.create({
+    trigger: 'aside .aside',
+    start: 'top top',
+    end: '+=250px',
+    animation: gsaptween,
+    onUpdate: () => {
+        document.querySelector('.gsapskill').style.color = 'linear-gradient(90deg, hsla(60, 56%, 91%, 1) 0%, hsla(81, 24%, 67%, 1) 10%, hsla(86, 21%, 58%, 1) 17%, hsla(90, 22%, 47%, 1) 39%, hsla(99, 44%, 39%, 1) 64%, hsla(101, 63%, 34%, 1) 86%, hsla(104, 99%, 28%, 1) 100%)'
+    },
     scrub: 1,
     markers: true
 })
@@ -67,11 +98,26 @@ ScrollTrigger.create({
     animation: goodtween,
     scrub: 1,
     onLeave: () => {
-        document.querySelector('.pgood').style.display = 'none'
+        document.querySelector('.pgood').style.opacity = 0,
+        document.querySelector('.pgood').style.transition = '0.3s ease'
     },
     onEnterBack: () => {
-        document.querySelector('.pgood').style.display = 'block'
+        document.querySelector('.pgood').style.opacity = 1
     },
+    markers: true
+})
+
+const arttween = gsap.to('.art', {
+    backgroundImage: 'linear-gradient(135deg, hsla(21, 100%, 50%, 1) 0%, hsla(32, 100%, 88%, 1) 100%)',
+    ease: 'power3.out'
+})
+
+ScrollTrigger.create({
+    trigger: 'aside .aside',
+    start: 'top top',
+    end: '+=300px',
+    animation: arttween,
+    scrub: 1,
     markers: true
 })
 
