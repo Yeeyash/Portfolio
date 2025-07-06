47f9d00c443d294b749417538e162f974c8f6b85
yeeYash <thakareyash99258@gmail.com>
2025-07-06 12:43:24 +0530

    JS.

--- script.js
+++ script.js
@@ -687,10 +687,192 @@ mm.add("(max-width: 470px)", () => {
 
     document.querySelector(".frontendskill .art").innerHTML = "<p>An Eye for Design and Art.</p>"
 
+    gsap.set(".shortinfo", {opacity: 0});
+
     let selfsplit = new SplitText('.selftaughtdiv', {type: 'words', wordsClass: 'word++'});
     let selfsplitted = selfsplit.words;
 
     gsap.set(selfsplitted, {display: 'inline-block'});
+
+    const shortinfoTween = gsap.to('.shortinfo', {
+        ease: 'power3.out',
+        opacity: 1
+    })
+
+    ScrollTrigger.create({
+        trigger: '.location',
+        start: 'top 30%',
+        end: '+=400px',
+        animation: shortinfoTween,
+        scrub: 1,
+        // markers: true
+    })
+
+    const colortween = gsap.to('.gsapskill', {
+        backgroundImage: 'linear-gradient(135deg, hsla(198, 59%, 82%, 1) 15%, hsla(198, 59%, 82%, 1) 23%, hsla(60, 57%, 91%, 1) 52%, hsla(44, 76%, 90%, 1) 76%, hsla(33, 100%, 88%, 1) 100%)',
+        ease: 'power1.out'
+    })
+
+    ScrollTrigger.create({
+        trigger: 'aside .aside',
+        start: 'top top',
+        end: '+=300px',
+        animation: colortween,
+        scrub: true,
+        // markers: true
+    })
+
+    const parenttween = gsap.to('.infocontainer', {
+        backgroundImage: 'linear-gradient(135deg, hsla(198, 59%, 82%, 1) 15%, hsla(198, 59%, 82%, 1) 23%, hsla(60, 57%, 91%, 1) 52%, hsla(44, 76%, 90%, 1) 76%, hsla(33, 100%, 88%, 1) 100%)',
+        ease: 'power1.out'
+    })
+
+    ScrollTrigger.create({
+        trigger: 'aside .aside',
+        start: 'top top',
+        end: '+=50px',
+        animation: parenttween,
+        scrub: 1,
+        // markers: true
+    })
+
+    const gsaptween = gsap.to('.pgsap', {
+        backgroundPosition: '100% 0%',
+        ease: 'power3.out'
+    })
+
+    ScrollTrigger.create({
+        trigger: 'aside .aside',
+        start: 'top top',
+        end: '+=250px',
+        animation: gsaptween,
+        onUpdate: () => {
+            document.querySelector('.pgsap').style.color = 'linear-gradient(90deg, hsla(60, 56%, 91%, 1) 0%, hsla(81, 24%, 67%, 1) 10%, hsla(86, 21%, 58%, 1) 17%, hsla(90, 22%, 47%, 1) 39%, hsla(99, 44%, 39%, 1) 64%, hsla(101, 63%, 34%, 1) 86%, hsla(104, 99%, 28%, 1) 100%)'
+        },
+        scrub: 1,
+        // markers: true
+    })
+
+    const goodcolortween = gsap.to('.goodat', {
+        backgroundImage: 'linear-gradient(90deg, hsla(198, 59%, 82%, 1) 15%, hsla(198, 59%, 82%, 1) 23%, hsla(60, 57%, 91%, 1) 52%, hsla(44, 76%, 90%, 1) 76%, hsla(33, 100%, 88%, 1) 100%)',
+        ease: 'power3.out'
+    })
+
+    ScrollTrigger.create({
+        trigger: '.location',
+        start: 'top top',
+        end: '+=200px',
+        animation: goodcolortween,
+        scrub: 1,
+        // markers: true
+    })
+
+    const arttween = gsap.to('.art', {
+        backgroundImage: 'linear-gradient(135deg, hsla(21, 100%, 50%, 1) 0%, hsla(32, 100%, 88%, 1) 100%)',
+        ease: 'power3.out'
+    })
+
+    ScrollTrigger.create({
+        trigger: 'aside .aside',
+        start: 'top top',
+        end: '+=300px',
+        animation: arttween,
+        scrub: 1,
+        // markers: true
+    })
+
+    const goodtween = gsap.to('.pgood', {
+        // right: '100px',
+        x: '20vw',
+        ease: 'power1.out'
+    })
+
+    ScrollTrigger.create({
+        trigger: '.location',
+        start: 'top top',
+        end: '+=400px',
+        animation: goodtween,
+        scrub: 1,
+        onLeave: () => {
+            document.querySelector('.pgood').style.opacity = 0,
+            document.querySelector('.pgood').style.transition = '0.3s ease'
+        },
+        onEnterBack: () => {
+            document.querySelector('.pgood').style.opacity = 1
+        },
+        // markers: true
+    })
+
+    document.fonts.ready.then(() => {
+
+        let mysplitText = new SplitText(".project", {type:'chars', tag: 'span'});
+        let chars = mysplitText.chars;
+
+        const projecttween = gsap.to('.project', {
+            // y: '210%',
+            // pin: true,
+            ease: 'power3.in'
+        });
+
+        ScrollTrigger.create({
+            trigger: '.gsapskill',
+            start: 'top top',
+            end: '+=230px',
+            animation: projecttween,
+            scrub: 1,
+            // markers: true
+        })
+
+        gsap.from(chars, {
+            stagger: 0.05,
+            y: 90,
+            ease: 'back.out',
+            duration: 1,
+            scrollTrigger: {
+                trigger: '.gsapskill',
+                start: 'top 10%',
+                end: '+=500px',
+                scrub: 1,
+                // markers: true
+            }
+        })
+
+        const finalcontainertween = gsap.to(".innerdiv", {
+            backgroundColor: "#730000",
+            transition: "0.3s ease-in"
+        })
+
+        ScrollTrigger.create({
+            trigger: ".unity",
+            start: 'top 30%',
+            end: '+=500px',
+            animation: finalcontainertween,
+            scrub: 1,
+        })
+
+
+
+        let splitLocation = new SplitText(".location, .course", {type: 'chars'});
+        let locsplit = splitLocation.chars;
+
+        gsap.from(locsplit, {
+            yPercent: 'random([-100, 100])',
+            rotation: 'random(-30, 30)',
+            ease: 'back.out',
+            autoAlpha: 0,
+            stagger: {
+                amount: 0.5,
+                from: 'random',
+            },
+            scrollTrigger: {
+                trigger: 'body',
+                start: 'top top',
+                end: '+=300px',
+                scrub: 1,
+                // markers: true
+            }
+        })
+    })
 
     // gsap.set('.innerdiv', {display: 'flex', visibility: 'visible', backgroundColor: '#730000'});
     //         document.querySelector('.innerdiv').style.flexGrow = 1;
